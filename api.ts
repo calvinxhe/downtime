9a9543348c4d4a2755215d5cfbe49a6485e47c82
calvinxhe <130882198+calvinxhe@users.noreply.github.com>
2023-12-12 22:30:07 -0500

    api

--- api.ts
+++ api.ts
@@ -7,7 +7,7 @@ export const getAllTodos = async (): Promise<ITask[]> => {
   const todos = await res.json();
   return todos;
 }
- 
+
 export const addTodo = async (todo: ITask): Promise<ITask> => {
   const res = await fetch(`${baseUrl}/tasks`, {
     method: 'POST',
